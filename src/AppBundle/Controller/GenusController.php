8dba3fbc699b46d0d92d932250ccd71c35bdc494
Manu <jgonzalezcantarero@gmail.com>
2017-04-29 00:26:20 +0200

    Show them a Genus, and the 404

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -42,26 +42,32 @@ class GenusController extends Controller
     }
 
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
-        $funFact = "Octopuses can change the color of their body in just *three-tenths* of a second!";
-        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
-        $key = md5($funFact);
-        if($cache->contains($key))
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name'=>$genusName]);
+        if(!$genus)
         {
-            $funFact = $cache->fetch($key);
+            throw $this->createNotFoundException('genus not found');
         }
-        else
-        {
-            sleep(1);
-            $funFact = $this->get('markdown.parser')->transform($funFact);
-            $cache->save($key, $funFact);
-        }
+
+//        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+//        $key = md5($funFact);
+//        if($cache->contains($key))
+//        {
+//            $funFact = $cache->fetch($key);
+//        }
+//        else
+//        {
+//            sleep(1);
+//            $funFact = $this->get('markdown.parser')->transform($funFact);
+//            $cache->save($key, $funFact);
+//        }
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName,
-            'funFact' => $funFact
+            'genus' => $genus
         ]);
     }
 
