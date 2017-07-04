1fffeaea45c0d6ac2460ddd78b9ad4668393f462
Manu <jgonzalezcantarero@gmail.com>
2017-07-04 21:47:12 +0200

    ManyToMany: The Inverse Side of the Relationship

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -70,7 +70,7 @@ class Genus
     private $slug;
 
     /**
-     * @ORM\ManyToMany(targetEntity="User")
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="studiedGenuses")
      * @ORM\JoinTable(name="genus_scientist")
      */
     private $genusScientists;
